4adcd974951bd2dadc74e8835ef83e45f5626a1a
Laura Fernandez <laurafcavia@gmail.com>
2024-04-15 20:43:03 +0200

    Refactor application interface loop into a class methods

--- lib/application.rb
+++ lib/application.rb
@@ -2,75 +2,94 @@ require_relative 'product'
 require_relative 'checkout'
 require_relative 'rule'
 
-# Seeding the available products in stock
-products = {
-  'GR1' => Product.new('GR1', 'Green Tea', 3.11),
-  'SR1' => Product.new('SR1', 'Strawberries', 5.00),
-  'CF1' => Product.new('CF1', 'Coffee', 11.23)
-}
+class Application
+  def initialize
+    @products = {
+      'GR1' => Product.new('GR1', 'Green Tea', 3.11),
+      'SR1' => Product.new('SR1', 'Strawberries', 5.00),
+      'CF1' => Product.new('CF1', 'Coffee', 11.23)
+    }
+    @checkout = Checkout.new(Rule.new)
+    @cart = []
+  end
 
-# Checkout is initialize with an instance of Rule, so it has access to the
-# different Rule methods defining the special pricing applied at checkout price calculation
-checkout = Checkout.new(Rule.new)
+  def start
+    puts "\n Welcome to the Good-Snacks online vending!"
+    loop do
+      display_options
+      action = gets.chomp.strip.upcase
+      break if action == 'D'
+      handle_action(action)
+    end
+    puts 'Thanks for shopping! Goodbye!'
+  end
 
-# Printing interface that gets user input
-puts "\n Welcome to the A-vending machine!"
-action = nil
-cart = []
-until action == 'D'
-  puts "\n ** Please enter letter to choose action. ** "
-  puts "\n A - See menu | B - Shop | C - Checkout | D - Exit "
+  private
 
-  action = gets.chomp.strip.upcase
-  case action
-  # Menu
-  when 'A'
+  def display_options
+    puts "\n ** Please enter letter to choose action. ** "
+    puts "\n A - See menu | B - Shop | C - Checkout | D - Exit "
+  end
+
+  def handle_action(action)
+    case action
+    when 'A'
+      display_menu
+    when 'B'
+      shop
+    when 'C'
+      checkout
+    else
+      puts 'Oops! Invalid input. Please try again.'
+    end
+  end
+
+  def display_menu
     puts "\n| Code | Name          | Price  |"
     puts '|------|---------------|--------|'
-    products.each_value do |product|
+    @products.each_value do |product|
       puts "| #{product.code.ljust(5)}| #{product.name.ljust(14)}| #{format('%.2f€', product.price).ljust(7)}|"
     end
     puts "\n** Special Discounts **"
     puts ' - Green Tea: buy-one-get-one-free'
     puts ' - Strawberries: 4.50€/each if you get 3 or more'
     puts ' - Coffee: 7.49€/each if you get 3 or more'
+  end
 
-  # Shop
-  when 'B'
-    # ADD PREVENTION FOR FAKE CODES
+  def shop
     puts "\n What would you like? Please enter the code."
     code_input = gets.chomp.strip.upcase
-
-    if products.key?(code_input)
+    if @products.key?(code_input)
       puts "\n How many?"
       n = gets.chomp.strip.to_i
       if n.integer? && n != 0
         input = [code_input] * n
         puts "\n Added #{n} x #{code_input}."
-        cart.concat(input)
+        @cart.concat(input)
       else
         puts 'Oops! Invalid number. Please try again.'
       end
     else
       puts 'Oops! Invalid product code. Please try again.'
     end
+  end
 
-  # Checkout
-  when 'C'
-    puts "\n| Checkout Summary#{''.ljust(3)}|"
-    puts "|#{'-' * 20}|"
+  def checkout
+    puts "\n| Checkout Summary#{''.ljust(4)}|"
+    puts "|#{'-' * 21}|"
     count = Hash.new(0)
-    cart.each { |code| count[code] += 1 }
+    @cart.each { |code| count[code] += 1 }
     count.each do |code, qty|
-      puts "| #{qty} x #{code.ljust(15)}|"
+      puts "| #{qty} x #{code.ljust(16)}|"
     end
-    puts "|#{'-' * 20}|"
-    cart.each do |code|
-      checkout.scan(products[code]) if products[code]
+    puts "|#{'-' * 21}|"
+    @cart.each do |code|
+      @checkout.scan(@products[code]) if @products[code]
     end
-    puts "| Total price: #{format('%.2f€', checkout.total)} |"
-  else
-    action == 'D' ? '' : (puts 'Oops! Invalid input. Please try again.')
+    puts "| Total price: #{format('%.2f€', @checkout.total)} |"
+    @cart.clear  # Clear cart after checkout
   end
 end
-puts 'Thanks for shopping! Goodbye 👋'
+
+app = Application.new
+app.start
